6b47490b6c83edb26f5e9c5df7b46d9131fd2e37
Scott Westover <scottwestover2006@gmail.com>
2023-01-10 22:07:37 -0500

    added initial class properties
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/connect-four.ts
+++ src/connect-four.ts
@@ -1,9 +1,15 @@
+export enum Player {
+  ONE = 'ONE',
+  TWO = 'TWO',
+}
+
+export type CellRange = 0 | 1 | 2;
+
 export default class ConnectFour {
-  // get the current board state
-  // get the current players turn
-  // check if the game is finished
-  // get the player that won the game
-  // get the cells that make up the winning connect four combination
-  // reset the game state
-  // add a game piece to a column
+  #board: CellRange[][] = [];
+  #playersTurn: Player = Player.ONE;
+  #isGameOver = false;
+  #gameWinner: undefined | Player;
+
+  constructor() {}
 }
